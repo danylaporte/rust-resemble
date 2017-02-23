68df08c16b87a18936dca5b78e64fd7cdda1d7b4
Dany Laporte <dany_laporte@hotmail.com>
2017-02-23 16:16:40 -0500

    fix invalid comparison and optimize

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,17 @@ extern crate test;
 
 use image::{DynamicImage, GenericImage, RgbaImage};
 use image::Pixel;
-use num_traits::sign::abs_sub;
 use num_traits::ToPrimitive;
 use rayon::prelude::*;
 use std::cmp::{max, min};
 use std::default::Default;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+type Rgba = image::Rgba<u8>;
+
 pub fn compare_images<I1, I2>(img1: &I1, img2: &I2, opt: &ComparisonOptions) -> Compare
-    where I1: GenericImage<Pixel = image::Rgba<u8>> + 'static + std::marker::Sync,
-          I2: GenericImage<Pixel = image::Rgba<u8>> + 'static + std::marker::Sync
+    where I1: GenericImage<Pixel = Rgba> + 'static + std::marker::Sync,
+          I2: GenericImage<Pixel = Rgba> + 'static + std::marker::Sync
 {
     let (width1, height1) = img1.dimensions();
     let (width2, height2) = img2.dimensions();
@@ -34,14 +35,11 @@ pub fn compare_images<I1, I2>(img1: &I1, img2: &I2, opt: &ComparisonOptions) -> 
         let y = index / width;
         let x = index - width * y;
         let pixel1 = img1.get_pixel(x, y);
-        let rgba1 = pixel_to_rgba(&pixel1);
-
         let pixel2 = img2.get_pixel(x, y);
-        let rgba2 = pixel_to_rgba(&pixel2);
-        let are_equals = compare_pixel(&rgba1, &rgba2, img1, img2, (x, y), opt);
+        let are_equals = compare_pixel(&pixel1, &pixel2, img1, img2, (x, y), opt);
 
         if are_equals {
-            *pixel = pixel1.to_rgba();
+            *pixel = pixel1;
         } else {
             *pixel = image::Rgba { data: [255, 0, 255, 255] };
             mismatch_count.fetch_add(1, Ordering::SeqCst);
@@ -81,10 +79,10 @@ impl ComparisonOptions {
 
     pub fn ignore_nothing(mut self) -> Self {
         self.ignore_antialiasing = false;
-        self.tolerance.alpha = 0.0;
-        self.tolerance.blue = 0.0;
-        self.tolerance.green = 0.0;
-        self.tolerance.red = 0.0;
+        self.tolerance.alpha = 0;
+        self.tolerance.blue = 0;
+        self.tolerance.green = 0;
+        self.tolerance.red = 0;
         self.tolerance.min_brightness = 0.0;
         self.tolerance.max_brightness = 255.0;
         self.ignore_antialiasing = false;
@@ -94,10 +92,10 @@ impl ComparisonOptions {
 
     pub fn ignore_less(mut self) -> Self {
         self.ignore_antialiasing = false;
-        self.tolerance.alpha = 16.0;
-        self.tolerance.blue = 16.0;
-        self.tolerance.green = 16.0;
-        self.tolerance.red = 16.0;
+        self.tolerance.alpha = 16;
+        self.tolerance.blue = 16;
+        self.tolerance.green = 16;
+        self.tolerance.red = 16;
         self.tolerance.min_brightness = 16.0;
         self.tolerance.max_brightness = 240.0;
         self.ignore_antialiasing = false;
@@ -107,10 +105,10 @@ impl ComparisonOptions {
 
     pub fn ignore_antialiasing(mut self) -> Self {
         self.ignore_antialiasing = false;
-        self.tolerance.alpha = 32.0;
-        self.tolerance.blue = 32.0;
-        self.tolerance.green = 32.0;
-        self.tolerance.red = 32.0;
+        self.tolerance.alpha = 32;
+        self.tolerance.blue = 32;
+        self.tolerance.green = 32;
+        self.tolerance.red = 32;
         self.tolerance.min_brightness = 64.0;
         self.tolerance.max_brightness = 96.0;
         self.ignore_antialiasing = true;
@@ -120,7 +118,7 @@ impl ComparisonOptions {
 
     pub fn ignore_colors(mut self) -> Self {
         self.ignore_antialiasing = false;
-        self.tolerance.alpha = 16.0;
+        self.tolerance.alpha = 16;
         self.tolerance.min_brightness = 16.0;
         self.tolerance.max_brightness = 240.0;
         self.ignore_antialiasing = false;
@@ -131,21 +129,13 @@ impl ComparisonOptions {
 
 #[derive(Default)]
 struct Tolerance {
-    alpha: f32,
+    alpha: u8,
     max_brightness: f32,
     min_brightness: f32,
-    red: f32,
-    green: f32,
-    blue: f32,
+    red: u8,
+    green: u8,
+    blue: u8,
 }
-
-struct Rgba {
-    r: f32,
-    g: f32,
-    b: f32,
-    a: f32,
-}
-
 fn compare_pixel<I1, I2>(pixel1: &Rgba,
                          pixel2: &Rgba,
                          img1: &I1,
@@ -153,30 +143,36 @@ fn compare_pixel<I1, I2>(pixel1: &Rgba,
                          position: (u32, u32),
                          opt: &ComparisonOptions)
                          -> bool
-    where I1: GenericImage<Pixel = image::Rgba<u8>>,
-          I2: GenericImage<Pixel = image::Rgba<u8>>
+    where I1: GenericImage<Pixel = Rgba>,
+          I2: GenericImage<Pixel = Rgba>
 {
-    if !is_similar(pixel1.a, pixel2.a, opt.tolerance.alpha) {
+    if !is_similar(pixel1.a() as i16,
+                   pixel2.a() as i16,
+                   opt.tolerance.alpha as i16) {
         false
     } else if opt.ignore_colors {
-        is_pixel_brightness_similar(&pixel1, &pixel2, &opt.tolerance)
-    } else if is_rgb_similar(&pixel1, &pixel2, &opt.tolerance) {
+        is_pixel_brightness_similar(pixel1, pixel2, &opt.tolerance)
+    } else if is_rgb_similar(pixel1, pixel2, &opt.tolerance) {
         true
     } else if opt.ignore_antialiasing &&
               (is_antialiased(pixel1, img1, &position, &opt.tolerance) ||
                is_antialiased(pixel2, img2, &position, &opt.tolerance)) {
-        true
+        is_pixel_brightness_similar(pixel1, pixel2, &opt.tolerance)
     } else {
         false
     }
 }
 
+fn abs_sub<T: num_traits::Signed + std::cmp::PartialOrd>(x: T, y: T) -> T {
+    if x < y { y - x } else { x - y }
+}
+
 fn get_brightness(rgba: &Rgba) -> f32 {
-    0.3 * rgba.r + 0.59 * rgba.g + 0.11 * rgba.b
+    0.3 * rgba.r() as f32 + 0.59 * rgba.g() as f32 + 0.11 * rgba.b() as f32
 }
 
 fn get_hue(rgba: &Rgba) -> f32 {
-    let (r, g, b) = (rgba.r, rgba.g, rgba.b);
+    let (r, g, b) = (rgba.r() as f32, rgba.g() as f32, rgba.b() as f32);
     let max = r.max(g).max(b);
     let min = r.min(g).min(b);
 
@@ -213,6 +209,8 @@ fn is_antialiased<I>(p1: &Rgba, image: &I, p: &(u32, u32), tolerance: &Tolerance
     let brightness1 = get_brightness(p1);
     let hue1 = get_hue(p1);
     let mut has_equivalent_sibling = 0;
+    let mut has_sibling_with_different_hue = 0;
+    let mut has_high_contrast_sibling = 0;
 
     for x in left..right {
         for y in top..bottom {
@@ -223,20 +221,23 @@ fn is_antialiased<I>(p1: &Rgba, image: &I, p: &(u32, u32), tolerance: &Tolerance
             }
 
             let p2 = image.get_pixel(x, y);
-            let p2 = pixel_to_rgba(&p2);
             let brightness2 = get_brightness(&p2);
             let hue2 = get_hue(&p2);
 
             if abs_sub(brightness1, brightness2) > tolerance.max_brightness {
-                return true;
+                has_high_contrast_sibling += 1;
             }
 
             if abs_sub(hue1, hue2) > 0.3 {
-                return true;
+                has_sibling_with_different_hue += 1;
             }
 
             if is_rgb_same(&p1, &p2) {
                 has_equivalent_sibling += 1;
+            }
+
+            if has_sibling_with_different_hue > 1 || has_high_contrast_sibling > 1 {
+                return true;
             }
         }
     }
@@ -247,30 +248,23 @@ fn is_antialiased<I>(p1: &Rgba, image: &I, p: &(u32, u32), tolerance: &Tolerance
 fn is_pixel_brightness_similar(p1: &Rgba, p2: &Rgba, tolerance: &Tolerance) -> bool {
     let brightness1 = get_brightness(p1);
     let brightness2 = get_brightness(p2);
-    is_similar(brightness1, brightness2, tolerance.min_brightness)
+    is_similar(brightness1 as f32,
+               brightness2 as f32,
+               tolerance.min_brightness)
 }
 
 fn is_rgb_same(p1: &Rgba, p2: &Rgba) -> bool {
-    p1.r == p2.r && p1.g == p2.g && p1.b == p2.b
+    p1.r() == p2.r() && p1.g() == p2.g() && p1.b() == p2.b()
 }
 
-fn is_similar(v1: f32, v2: f32, tolerance: f32) -> bool {
+fn is_similar<T: num_traits::Signed + std::cmp::PartialOrd>(v1: T, v2: T, tolerance: T) -> bool {
     abs_sub(v1, v2) <= tolerance
 }
 
 fn is_rgb_similar(p1: &Rgba, p2: &Rgba, t: &Tolerance) -> bool {
-    is_similar(p1.r, p2.r, t.red) && is_similar(p1.g, p2.g, t.green) &&
-    is_similar(p1.b, p2.b, t.blue)
-}
-
-fn pixel_to_rgba<P: Pixel>(pixel: &P) -> Rgba {
-    let rgb = pixel.to_rgba();
-    Rgba {
-        r: rgb.data[0].to_f32().unwrap(),
-        g: rgb.data[1].to_f32().unwrap(),
-        b: rgb.data[2].to_f32().unwrap(),
-        a: rgb.data[3].to_f32().unwrap(),
-    }
+    is_similar(p1.r() as i16, p2.r() as i16, t.red as i16) &&
+    is_similar(p1.g() as i16, p2.g() as i16, t.green as i16) &&
+    is_similar(p1.b() as i16, p2.b() as i16, t.blue as i16)
 }
 
 #[cfg(test)]
@@ -290,5 +284,30 @@ mod tests {
         let opts = &ComparisonOptions::new();
 
         b.iter(|| compare_images(img1, img2, opts));
+    }
+}
+
+trait RgbaEx {
+    fn r(&self) -> u8;
+    fn g(&self) -> u8;
+    fn b(&self) -> u8;
+    fn a(&self) -> u8;
+}
+
+impl RgbaEx for Rgba {
+    fn r(&self) -> u8 {
+        self.data[0]
+    }
+
+    fn g(&self) -> u8 {
+        self.data[1]
+    }
+
+    fn b(&self) -> u8 {
+        self.data[2]
+    }
+
+    fn a(&self) -> u8 {
+        self.data[3]
     }
 }
